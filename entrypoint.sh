383514de91e83190d88c4becbf912efa65b0a2d0
shitarahiroka <rem.moon4@gmail.com>
2023-04-25 16:48:24 +0900

    Dockerfile編集

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -e
+#set -e
 
 # 自分のアプリに合わせて必要なコマンドを修正してください
-bin/rails db:migrate
-bin/rails db:seed
+#bin/rails db:migrate
+#bin/rails db:seed
 
-rm -f tmp/pids/server.pid && bin/rails s
+#rm -f tmp/pids/server.pid && bin/rails s
